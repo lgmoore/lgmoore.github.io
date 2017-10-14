9ba262613e1f5843cf42f95e271befb2c854cf36
lgmoore <32501660+lgmoore@users.noreply.github.com>
2017-10-14 16:35:36 +1100

    hope this works
    javascript album test

--- java/music_pop.js
+++ java/music_pop.js
@@ -11,27 +11,29 @@ function hidediscWin () {
 /* Get id of clicked */
 function reply_click(clicked_id)
 {
+	showdiscWin();
+
 	var img = document.createElement("img");
 
     if (clicked_id == "highway61_btn") {
     		img.src = "images/highway61.jpg";
     }
 
-	var src = document.getElementById("add_img");
-	src.appendChild(img);
+	var xsrc = document.getElementById("add_img");
+	xsrc.appendChild(img);
 }
 
 /* Main Function */
 function show_window () {
 	console.log("window loaded");
 	/* Links */
-	highwayBtn = document.getElementById("highway61_btn");
+	album = document.getElemenyByClass("album");
 	discWinClose = document.getElementById("discWinClose");
 	discWin = document.getElementById("discWin");
 	scrnOverlay = document.getElementById("scrnOverlay");
 
 /* Events */
-  highwayBtn.onclick = reply_click(this.id)
+  album.onclick = reply_click(this.id)
   discWinClose.onclick = hidediscWin;
 }
 /* On-Load */
